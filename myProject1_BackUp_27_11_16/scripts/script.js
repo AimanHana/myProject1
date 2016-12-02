3e63f135535bdcd89a8f72cfc1ca6f4f306dea61
Aiman Hana <aimanhana7@gmail.com>
2016-12-02 02:58:21 +0000

    Added form/sign up and sign in buttons

--- myProject1_BackUp_27_11_16/scripts/script.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function(){
-	$("h1").click(function(){
-		alert("you clicked me");
-	});
-});
--- scripts/script.js
+++ scripts/script.js
@@ -2,4 +2,26 @@ $(document).ready(function(){
 	$("h1").click(function(){
 		alert("you clicked me");
 	});
+	
+	// $("#yes").click(function(){
+	// 	alert("you clicked me");
+	// 	$("#phonePlace").attr("visibility","visible");	
+	// });
+	
+	// $("#no").click(function(){
+	// 	alert("you clicked me");
+	// 	$("#phonePlace").attr("visibility","hidden");	
+	// });
 });
+
+function showPhone(){
+	//alert("You clicked me");
+	document.getElementById("phonePlace").style.display="block";
+	document.getElementById("phonePlace").style.visibility="visible";
+}
+
+function hidePhone(){
+	//alert("You clicked me");
+	document.getElementById("phonePlace").style.visibility="hidden";
+	document.getElementById("phonePlace").style.display="none";	
+}
